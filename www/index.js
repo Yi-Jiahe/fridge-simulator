1e068955f3403572f1d616ac63939c30e8151a51
Jiahe <yi.jiahe00@gmail.com>
2022-09-04 20:45:01 +0800

    update scaling and axes

--- www/index.js
+++ www/index.js
@@ -18,8 +18,8 @@ const drawTemperatures = () => {
 
     // set the dimensions and margins of the graph
     const margin = {top: 10, right: 40, bottom: 30, left: 30},
-    width = 1440 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 1440 + margin.left + margin.right,
+    height = 400 + margin.top + margin.bottom;
 
     // append the svg object to the body of the page
     let svg = d3.select('svg');
@@ -27,10 +27,11 @@ const drawTemperatures = () => {
     svg.attr('width', width)
         .attr('height', height);
 
-    let scale = d3.scaleLinear().domain([0, 100]).range([0, 400]);
+    let scaleX = d3.scaleLinear().domain([0, 1440]).range([0, 1440]);
+    let scaleY = d3.scaleLinear().domain([30, -10]).range([0, 400]);
 
-    let axisLeft = d3.axisLeft(scale);
-    let axisBottom = d3.axisBottom(scale);
+    let axisBottom = d3.axisBottom(scaleX);
+    let axisLeft = d3.axisLeft(scaleY);
     
     d3.select('#left').call(axisLeft);
     d3.select('#bottom').call(axisBottom);
@@ -38,8 +39,8 @@ const drawTemperatures = () => {
     let points = svg.selectAll('circle')
         .data(history)
         .join('circle')
-        .attr('cx', function(_, i) { return i; })
-        .attr('cy', function(d, _) { return -d + 300; })
+        .attr('cx', function(_, i) { return i+30; })
+        .attr('cy', function(d, _) { return -d*10 + 300; })
         .attr('r', 1);
 }
 
