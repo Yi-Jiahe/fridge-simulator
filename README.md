70352a8911b8e42ccd103bf76c96f28241e80055
Jiahe <yi.jiahe00@gmail.com>
2022-09-04 17:59:55 +0800

    Implement simulation w/ given assumptions

--- README.md
+++ README.md
@@ -1,73 +1,16 @@
-<div align="center">
+# Fridge simulator
 
-  <h1><code>wasm-pack-template</code></h1>
-
-  <strong>A template for kick starting a Rust and WebAssembly project using <a href="https://github.com/rustwasm/wasm-pack">wasm-pack</a>.</strong>
-
-  <p>
-    <a href="https://travis-ci.org/rustwasm/wasm-pack-template"><img src="https://img.shields.io/travis/rustwasm/wasm-pack-template.svg?style=flat-square" alt="Build Status" /></a>
-  </p>
-
-  <h3>
-    <a href="https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/index.html">Tutorial</a>
-    <span> | </span>
-    <a href="https://discordapp.com/channels/442252698964721669/443151097398296587">Chat</a>
-  </h3>
-
-  <sub>Built with 🦀🕸 by <a href="https://rustwasm.github.io/">The Rust and WebAssembly Working Group</a></sub>
-</div>
-
-## About
-
-[**📚 Read this template tutorial! 📚**][template-docs]
-
-This template is designed for compiling Rust libraries into WebAssembly and
-publishing the resulting package to NPM.
-
-Be sure to check out [other `wasm-pack` tutorials online][tutorials] for other
-templates and usages of `wasm-pack`.
-
-[tutorials]: https://rustwasm.github.io/docs/wasm-pack/tutorials/index.html
-[template-docs]: https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/index.html
-
-## 🚴 Usage
-
-### 🐑 Use `cargo generate` to Clone this Template
-
-[Learn more about `cargo generate` here.](https://github.com/ashleygwilliams/cargo-generate)
-
-```
-cargo generate --git https://github.com/rustwasm/wasm-pack-template.git --name my-project
-cd my-project
-```
-
-### 🛠️ Build with `wasm-pack build`
-
-```
-wasm-pack build
-```
-
-### 🔬 Test in Headless Browsers with `wasm-pack test`
-
-```
-wasm-pack test --headless --firefox
-```
-
-### 🎁 Publish to NPM with `wasm-pack publish`
-
-```
-wasm-pack publish
-```
-
-## 🔋 Batteries Included
-
-* [`wasm-bindgen`](https://github.com/rustwasm/wasm-bindgen) for communicating
-  between WebAssembly and JavaScript.
-* [`console_error_panic_hook`](https://github.com/rustwasm/console_error_panic_hook)
-  for logging panic messages to the developer console.
-* [`wee_alloc`](https://github.com/rustwasm/wee_alloc), an allocator optimized
-  for small code size.
-* `LICENSE-APACHE` and `LICENSE-MIT`: most Rust projects are licensed this way, so these are included for you
+## Assumptions
+- Air properties are constant
+  - Mass of air is constant
+    - Ignoring exchange of cold/ hot air when fridge is opened
+    - Density is taken to be at the average of air at 12°C 1atm
+  - Heat capacity of air is constant
+- Rate of heat gain by fridge is constant
+  - Temperature differential between fridge and outside is constant @ ~23deg°C (2°C and 25°C(rtp))
+  - Estimates from [Experimental Evaluation of the Heat Transfer
+Through the Walls of Household Refrigerators](https://docs.lib.purdue.edu/cgi/viewcontent.cgi?article=1501&context=iracc)
+- Cooling for refrigerator system is constant when on
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::collections::VecDeque;
+
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -8,12 +10,97 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// kg/m^3
+const DENSITY_AIR: f32 = 1.225;
+// J per kg per °K
+// Assume constant volume of fridge
+const C_V_AIR: f32 = 700.0;
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Simulation {
+    // Cubic meters 
+    capacity: f32,
+    history: Vec<f32>,
+    history_queue: VecDeque<f32>,
+    set_point: f32,
+    refrigeration_on: bool,
+    // W
+    rate_of_heat_gain: f32,
+    // W
+    rate_of_cooling: f32,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, fridge-simulator!");
+impl Simulation {
+    pub fn new(capacity: f32, initial_temperature: f32) -> Simulation {
+        let vol_ratio = capacity/0.220;
+        let length_ratio = vol_ratio.powf(1.0/3.0);
+        // Estimated using a value of 47.00W for a 220L fridge
+        // Assuming the surface area can be estimated using the cube root of the volume ratio
+        let rate_of_heat_gain = 47.00*length_ratio;
+
+        // Pulled this value of of nowhere
+        let rate_of_cooling = 40.0;
+
+        Simulation{
+            capacity,
+            history: vec![initial_temperature],
+            history_queue: VecDeque::from([initial_temperature]),
+            set_point: 2.0,
+            refrigeration_on: false,
+            rate_of_heat_gain,
+            rate_of_cooling: rate_of_cooling,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let current_temp = *self.history_queue.back().unwrap();
+
+        let mut dE_dot = self.rate_of_heat_gain;
+
+        if self.refrigeration_on {
+            if current_temp < (self.set_point - 2.0) {
+                self.refrigeration_on = false;
+            }
+        } else {
+            if current_temp > (self.set_point + 2.0) {
+                self.refrigeration_on = true;
+            }
+        }
+
+        if self.refrigeration_on {
+            dE_dot -= self.rate_of_cooling;
+        }
+
+        let mass_air = self.capacity * DENSITY_AIR;
+        let new_temp = current_temp + dE_dot*60.0/(mass_air*C_V_AIR);
+
+        self.history_queue.push_back(new_temp);
+        if self.history_queue.len() > 1440 {
+            self.history_queue.pop_front();
+        }
+
+        self.history_queue.make_contiguous();
+        self.history = self.history_queue.as_slices().0.to_vec();
+    }
+
+    pub fn rate_of_heat_gain(&self) -> f32 {
+        self.rate_of_heat_gain
+    }
+
+    pub fn refrigeration_on(&self) -> bool {    
+        self.refrigeration_on
+    }
+
+    pub fn history(&self) -> *const f32 {
+        self.history.as_ptr()
+    }
+
+    pub fn len_history(&self) -> i32 {
+        self.history.len() as i32
+    }
+
+    pub fn current_temp(&self) -> f32 {
+        *self.history_queue.back().unwrap()
+    }
 }
